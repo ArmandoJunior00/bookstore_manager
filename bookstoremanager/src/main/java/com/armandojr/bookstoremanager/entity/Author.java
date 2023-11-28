f172b5ae53c8523711ed62efbe73e37922034220
ArmandoJunior00 <armaanijr00@gmail.com>
2023-11-28 15:32:26 -0300

    adicionado as entidades

---
+++ bookstoremanager/src/main/java/com/armandojr/bookstoremanager/entity/Author.java
@@ -0,0 +1,21 @@
+package com.armandojr.bookstoremanager.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private Integer age;
+}
